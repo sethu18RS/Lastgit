4731af5561e7fa3583c640c3d5c9bfd63252f8bb
Admin <sethurajendiran18@gmail.com>
2026-05-02 13:36:27 +0530

    this is commit from asian timezone

--- src/programming/sixttothreero.java
+++ src/programming/sixttothreero.java
@@ -5,6 +5,9 @@ public class sixttothreero {
 		for(int i=66;i>=32;i--)
 		{
 			System.out.println(i);
+			System.out.println("ram0");
+			System.out.println("ram1");
+			System.out.println("ram2");
 		}
 	}
 
